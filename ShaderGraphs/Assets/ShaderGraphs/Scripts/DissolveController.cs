c6ed950539639e5c8e04155e1249175c348346e0
JeanCho <nicetoto97@naver.com>
2019-10-31 14:37:24 +0900

    Done Shader Examples

---
+++ ShaderGraphs/Assets/ShaderGraphs/Scripts/DissolveController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveController : MonoBehaviour
+{
+
+    public Material dissolveMaterial;
+    public float dissolveAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.O))
+        {
+            dissolveAmount += 0.05f;
+            dissolveMaterial.SetFloat("Vector1_371CFFC1", dissolveAmount);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            dissolveAmount -= 0.05f;
+            dissolveMaterial.SetFloat("Vector1_371CFFC1", dissolveAmount);
+
+        }
+    }
+}
